2e54d186ef11c9009b344c132fac2bec22c4e0c7
Masato Mouri <13274198+mouri111@users.noreply.github.com>
2019-10-24 20:28:12 +0900

    Add test for unify and Fix

--- src/unify.rs
+++ src/unify.rs
@@ -4,10 +4,9 @@ use std::collections::VecDeque;
 use std::path::PathBuf;
 use std::fs::read_to_string;
 
-pub fn unify_code(s: &str) -> String {
+pub fn unify_code(config: &Config, s: &str) -> String {
     let mut btree = BTreeMap::new();
     let mut queue = VecDeque::new();
-    let config = read_config();
     for line in s.lines() {
         if line.starts_with("use ") {
             let crate_name: String = line.chars().skip(4).take_while(|&c| c != ':' && c != ';').collect();
@@ -28,11 +27,13 @@ pub fn unify_code(s: &str) -> String {
         }
     }
     let mut res = String::new();
-    for (_, unified) in &btree {
+    for (crate_name, unified) in &btree {
+        res.push_str(&format!("mod {} {{\n", crate_name));
         res.push_str(unified);
+        res.push_str("}\n")
     }
     res.push_str(s);
-    res
+    normalize(&res)
 }
 
 fn unify_crate(config: &Config, crate_name: &str) -> (String, Vec<String>) {
@@ -51,6 +52,56 @@ fn unify_crate(config: &Config, crate_name: &str) -> (String, Vec<String>) {
             deps.push(crate_name);
         }
         res.push_str(line);
+        res.push('\n');
     }
     (res, deps)
+}
+
+pub fn normalize(s: &str) -> String {
+    let mut res = String::new();
+    let mut prev_is_empty_line = true;
+    for line in s.lines() {
+        if line.len() == 0 {
+            if ! prev_is_empty_line {
+                res.push_str(line);
+                res.push('\n');
+                prev_is_empty_line = true;
+            }
+        }
+        else {
+            res.push_str(line);
+            res.push('\n');
+        }
+    }
+    res
+}
+
+#[test]
+fn test_unify() {
+    use std::collections::BTreeMap;
+    use std::str::FromStr;
+    let s = r#"
+use test_crate::test_function;
+fn main() {
+    println!("test");
+    assert_eq!(test_function(1), 2);
+}"#;
+    let mut btree = BTreeMap::new();
+    btree.insert("test_crate".to_string(), CrateInfo { path: PathBuf::from_str("test_data/test-crate").unwrap() });
+    let config = Config {
+        crates: Some(btree),
+    };
+    let res = unify_code(&config, &s);
+    let right = r#"
+mod test_crate {
+pub fn test_function(x: i64) -> i64 {
+    x + 1
+}
+}
+use test_crate::test_function;
+fn main() {
+    println!("test");
+    assert_eq!(test_function(1), 2);
+}"#;
+    assert_eq!(res, normalize(right));
 }
---
+++ test_data/test-crate/src/lib.rs
@@ -0,0 +1,3 @@
+pub fn test_function(x: i64) -> i64 {
+    x + 1
+}
