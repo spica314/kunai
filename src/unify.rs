e152886a68ec9314728a7cdf6e4f5101c4b891c3
Masato Mouri <13274198+mouri111@users.noreply.github.com>
2019-10-30 19:53:26 +0900

    Rewrie src/unify.rs

--- src/unify.rs
+++ src/unify.rs
@@ -1,79 +1,133 @@
-use std::collections::BTreeMap;
+use std::collections::BTreeSet;
 use crate::config::*;
-use std::collections::VecDeque;
 use std::path::PathBuf;
 use std::fs::read_to_string;
 
-pub fn unify_code(config: &Config, s: &str) -> String {
-    let mut btree = BTreeMap::new();
-    let mut queue = VecDeque::new();
-    for line in s.lines() {
-        if line.starts_with("use ") {
-            let crate_name: String = line.chars().skip(4).take_while(|&c| c != ':' && c != ';').collect();
-            if crate_name == "std" || crate_name == "core" {
-                continue;
-            }
-            queue.push_back(crate_name);
-        }
+pub fn unify(bin_name: &Option<&str>, rust_2018: bool) -> String {
+    if bin_name.is_none() {
+        unimplemented!();
     }
-    while let Some(crate_name) = queue.pop_front() {
-        if btree.contains_key(&crate_name) {
-            continue;
-        }
-        let (unified, deps) = unify_crate(&config, &crate_name);
-        btree.insert(crate_name, unified);
-        for d in deps {
-            queue.push_back(d);
-        }
-    }
-    let mut res = String::new();
-    for (crate_name, unified) in &btree {
-        res.push_str(&format!("mod {} {{\n", crate_name));
-        res.push_str(unified);
-        res.push_str("}\n")
-    }
-    res.push_str(s);
-    normalize(&res)
-}
-
-fn unify_crate(config: &Config, crate_name: &str) -> (String, Vec<String>) {
-    let mut pathbuf = config.crates.as_ref().unwrap().get(crate_name).unwrap().path.clone();
+    let config = read_config();
+    let mut pathbuf = cargo_edit::find(&None).unwrap();
+    pathbuf.pop();
     pathbuf.push("src");
-    pathbuf.push("lib.rs");
-    let s = read_to_string(&pathbuf).unwrap();
-    let mut res = String::new();
-    let mut deps = vec![];
-    for line in s.lines() {
-        if line.starts_with("use ") {
-            let crate_name: String = line.chars().skip(4).take_while(|&c| c != ':' && c != ';').collect();
-            if crate_name == "std" || crate_name == "core" {
-                continue;
-            }
-            deps.push(crate_name);
-        }
-        res.push_str(line);
-        res.push('\n');
-    }
-    (res, deps)
+    pathbuf.push("bin");
+    pathbuf.push(&format!("{}.rs", bin_name.unwrap()));
+    eprintln!("pathbuf = {:?}", pathbuf);
+    let code = read_to_string(&pathbuf).unwrap();
+    unify_code2(&config, rust_2018, &code, &pathbuf)
 }
 
-pub fn normalize(s: &str) -> String {
+fn unify_code2(config: &Config, rust_2018: bool, s: &str, path: &PathBuf) -> String {
+    let mut expanded = BTreeSet::new();
+    let mut macro_use = BTreeSet::new();
+    let mut crate_texts = vec![];
+    dfs(config, rust_2018, s, &mut expanded, &mut macro_use, &mut crate_texts, "", path);
     let mut res = String::new();
-    let mut prev_is_empty_line = true;
-    for line in s.lines() {
-        if line.len() == 0 {
-            if ! prev_is_empty_line {
-                res.push_str(line);
+    let mut expanded_flag = false;
+    for line in crate_texts[crate_texts.len()-1].1.lines() {
+        if !expanded_flag && line.starts_with("use") {
+            for (crate_name, text) in &crate_texts {
+                if crate_name == "" {
+                    continue;
+                }
+                if macro_use.contains(crate_name) {
+                    res.push_str("#[macro_use]");
+                    res.push('\n');
+                }
+                res.push_str(&format!("mod {} {{", crate_name));
                 res.push('\n');
-                prev_is_empty_line = true;
+                res.push_str(text);
+                res.push_str("}");
+                res.push('\n');
+                res.push('\n');
             }
+            res.push_str(line);
+            res.push('\n');
+            expanded_flag = true;
         }
         else {
             res.push_str(line);
             res.push('\n');
         }
     }
-    res
+    eprintln!("prev");
+    eprintln!("{}", res);
+    let mut buf = vec![];
+    let mut config = rustfmt_nightly::Config::default();
+    config.set().emit_mode(rustfmt_nightly::EmitMode::Stdout);
+    config.set().verbose(rustfmt_nightly::Verbosity::Quiet);
+    let mut session = rustfmt_nightly::Session::new(config, Some(&mut buf));
+    session.format(rustfmt_nightly::Input::Text(res)).ok();
+    drop(session);
+    std::str::from_utf8(&buf).unwrap().to_string()
+}
+
+fn dfs(
+    config: &Config,
+    rust_2018: bool,
+    s: &str, 
+    expanded: &mut BTreeSet<String>,
+    macro_use: &mut BTreeSet<String>, 
+    crate_texts: &mut Vec<(String,String)>, 
+    my_name: &str,
+    my_path: &PathBuf,
+) {
+    if expanded.contains(my_name) {
+        return;
+    }
+    let mut flag_macro_use = false;
+    let mut res = String::new();
+    for line in s.lines() {
+        if line.starts_with("use ") {
+            let crate_name: String = line.chars().skip(4).take_while(|&c| c != ':' && c != ';').collect();
+            if crate_name == "std" || crate_name == "core" || crate_name == "crate" {
+                res.push_str(line);
+                res.push('\n');
+                continue;
+            }
+            let mut pathbuf = config.get_crate_path(&crate_name).unwrap();
+            pathbuf.push("src");
+            pathbuf.push("lib.rs");
+            let code = read_to_string(&pathbuf).unwrap();
+            dfs(config, rust_2018, &code, expanded, macro_use, crate_texts, &crate_name, &pathbuf);
+            if rust_2018 {
+                let line2 = line.replace("use ", "use crate::");
+                res.push_str(&line2);
+                res.push('\n');
+            }
+            else {
+                let line2 = line.replace("use ", "use ::");
+                res.push_str(&line2);
+                res.push('\n');
+            }
+            flag_macro_use = false;
+        }
+        else if line.starts_with("#[macro_use]") {
+            flag_macro_use = true;
+        }
+        else if line.starts_with("extern crate ") {
+            let crate_name: String = line.chars().skip(13).take_while(|&c| c != ';').collect();
+            if crate_name == "std" || crate_name == "core" || crate_name == "crate" {
+                panic!("unable to expand '{}'", line);
+            }
+            let mut pathbuf = config.get_crate_path(&crate_name).unwrap();
+            pathbuf.push("src");
+            pathbuf.push("lib.rs");
+            let code = read_to_string(&pathbuf).unwrap();
+            dfs(config, rust_2018, &code, expanded, macro_use, crate_texts, &crate_name, &pathbuf);
+            if flag_macro_use {
+                macro_use.insert(crate_name.clone());
+            }
+            flag_macro_use = false;
+        }
+        else {
+            res.push_str(line);
+            res.push('\n');
+        }
+    }
+    expanded.insert(my_name.to_string());
+    crate_texts.push((my_name.to_string(), res));
 }
 
 #[test]
@@ -91,17 +145,18 @@ fn main() {
     let config = Config {
         crates: Some(btree),
     };
-    let res = unify_code(&config, &s);
-    let right = r#"
-mod test_crate {
-pub fn test_function(x: i64) -> i64 {
-    x + 1
+    let res = unify_code2(&config, false, &s, &PathBuf::new());
+    let right = r#"mod test_crate {
+    pub fn test_function(x: i64) -> i64 {
+        x + 1
+    }
 }
-}
+
 use test_crate::test_function;
 fn main() {
     println!("test");
     assert_eq!(test_function(1), 2);
-}"#;
-    assert_eq!(res, normalize(right));
+}
+"#;
+    assert_eq!(res, right);
 }
