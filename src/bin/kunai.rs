14b7e625ae9d97f7a8feacfee19c34e0487beac1
Masato Mouri <13274198+mouri111@users.noreply.github.com>
2019-10-30 19:56:32 +0900

    Update src/bin/kunai.rs

--- src/bin/kunai.rs
+++ src/bin/kunai.rs
@@ -1,3 +1,48 @@
+#[macro_use]
+extern crate clap;
+
+use clap::{App, Arg, SubCommand};
+use kunai::unify::*;
+use kunai::config::*;
+
 fn main() {
-    println!("Hello, world!");
+    let app = App::new("kunai")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .subcommand(SubCommand::with_name("unify")
+            .arg(Arg::with_name("binname")
+                .value_name("binname")
+                .required(true)
+            )
+        )
+        .subcommand(SubCommand::with_name("crate")
+            .subcommand(SubCommand::with_name("add")
+                .arg(Arg::with_name("path")
+                    .value_name("path")
+                    .required(true)
+                )
+            )
+        );
+    let matches = app.clone().get_matches();
+    if let Some(unify_matches) = matches.subcommand_matches("unify") {
+        let binname = unify_matches.value_of("binname");
+        let res = unify(&binname, true);
+        println!("{}", res);
+    }
+    else if let Some(crate_matches) = matches.subcommand_matches("crate") {
+        if let Some(crate_add_matches) = crate_matches.subcommand_matches("add") {
+            let path = crate_add_matches.value_of("path").unwrap();
+            let mut config = read_config();
+            config.add_crate(&path);
+            write_config(&config);
+        }
+        else {
+            unimplemented!();
+        }
+    }
+    else {
+        let mut app = app;
+        app.print_help().ok();
+        println!();
+    }
 }
