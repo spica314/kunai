53595160a42a0de35a2a3a5ea71e35bc8c509284
Masato Mouri <13274198+mouri111@users.noreply.github.com>
2019-11-03 21:01:11 +0900

    Add --rust2015 flag for old judge

--- src/bin/kunai.rs
+++ src/bin/kunai.rs
@@ -9,17 +9,27 @@ fn main() {
         .version(crate_version!())
         .author(crate_authors!())
         .subcommand(
-            SubCommand::with_name("unify").arg(
+            SubCommand::with_name("unify")
+            .arg(
                 Arg::with_name("binname")
                     .value_name("binname")
                     .required(true),
-            ),
+            )
+            .arg(
+                Arg::with_name("rust2015_flag")
+                    .long("rust2015")
+            )
         );
     let matches = app.clone().get_matches();
     if let Some(unify_matches) = matches.subcommand_matches("unify") {
         let binname = unify_matches.value_of("binname");
-        let res = unify(&binname, true);
-        println!("{}", res);
+        if unify_matches.is_present("rust2015_flag") {
+            let res = unify(&binname, false);
+            println!("{}", res);
+        } else {
+            let res = unify(&binname, true);
+            println!("{}", res);
+        }
     } else {
         let mut app = app;
         app.print_help().ok();
