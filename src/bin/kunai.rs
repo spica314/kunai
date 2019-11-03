26d31520c310015db850dc1275364a9c53b40c5c
Masato Mouri <13274198+mouri111@users.noreply.github.com>
2019-11-03 20:15:16 +0900

    Delete unused code

--- src/bin/kunai.rs
+++ src/bin/kunai.rs
@@ -14,21 +14,6 @@ fn main() {
                     .value_name("binname")
                     .required(true),
             ),
-        )
-        .subcommand(
-            SubCommand::with_name("crate")
-                .subcommand(
-                    SubCommand::with_name("add")
-                        .arg(Arg::with_name("path").value_name("path").required(true)),
-                )
-                .subcommand(
-                    SubCommand::with_name("remove").arg(
-                        Arg::with_name("crate-name")
-                            .value_name("crate-name")
-                            .required(true),
-                    ),
-                )
-                .subcommand(SubCommand::with_name("list")),
         );
     let matches = app.clone().get_matches();
     if let Some(unify_matches) = matches.subcommand_matches("unify") {
