572c3e159554e9045103ed43f962638f396b17e6
Masato Mouri <13274198+mouri111@users.noreply.github.com>
2019-12-30 00:13:31 +0900

    Delete temp.rs

--- src/bin/temp.rs
+++
@@ -1,8 +0,0 @@
-use std::io::Read;
-
-fn main() {
-    let mut buffer = String::new();
-    std::io::stdin().read_to_string(&mut buffer).unwrap();
-    let res = kunai::atcoder::get_tests_from_html(&buffer);
-    eprintln!("res = {:?}", res);
-}
